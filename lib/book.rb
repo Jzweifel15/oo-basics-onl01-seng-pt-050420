a68a90d07da1436ba4b837d4a950fed24dd9fcfd
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-19 16:59:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,13 @@
 class Book
   
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
   
   def title
     return @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author 
-    return @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    return @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    return @genre
   end
   
   def turn_page
